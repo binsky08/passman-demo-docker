e975d776b57516ee712ac85cac9a292bfdeadf08
binsky <timo@binsky.org>
2021-08-01 23:42:03 +0200

    check for existing installation at container start

--- docker_demo_start.sh
+++ docker_demo_start.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-NEXTCLOUD_ADMIN_PASSWORD=admin NEXTCLOUD_ADMIN_USER=admin SQLITE_DATABASE=True NEXTCLOUD_TRUSTED_DOMAINS="localhost passman.cc demo.passman.cc" NEXTCLOUD_UPDATE=1 /entrypoint.sh echo "Installation done"
+if [ ! -f /var/www/html/config/config.php ]
+then
+    NEXTCLOUD_ADMIN_PASSWORD=admin NEXTCLOUD_ADMIN_USER=admin SQLITE_DATABASE=True NEXTCLOUD_TRUSTED_DOMAINS="localhost passman.cc demo.passman.cc" NEXTCLOUD_UPDATE=1 /entrypoint.sh echo "Installation done"
 
-su -p www-data -s /bin/sh -c "cd /var/www/html/ php occ check &&
-    php occ status &&
-    php occ app:list &&
-    php occ app:install passman &&
-    php occ app:disable firstrunwizard &&
-    php occ upgrade &&
-    php occ config:system:set defaultapp --value=passman"
+    su -p www-data -s /bin/sh -c "cd /var/www/html/ php occ check &&
+        php occ status &&
+        php occ app:list &&
+        php occ app:install passman &&
+        php occ app:disable firstrunwizard &&
+        php occ upgrade &&
+        php occ config:system:set defaultapp --value=passman"
+fi
 
 exec apache2-foreground
