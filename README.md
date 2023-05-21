eea3da9978bc810adba9565fd5f2f0866ba5c629
binsky <timo@binsky.org>
2023-05-21 14:46:20 +0200

    improve preconfiguration script and README.md

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-passman demo docker alpine
+Passman demo docker container setup
 ===
 
-remove container with `docker rm <containerId> --volumes` so that the associated volume (containing the nextcloud installation) will be removed automatically
+Build with `docker build -t passman-demo-docker:latest .`
+
+Start with `docker run -d -p 8080:80 -p 8443:443 --name my-passman-demo-docker passman-demo-docker:latest`
+
+Stop / Kill with `docker kill my-passman-demo-docker`
+
+Remove container with `docker rm my-passman-demo-docker --volumes` so that the associated volume (containing the nextcloud installation) will be removed automatically
--- docker_demo_start.sh
+++ docker_demo_start.sh
@@ -2,15 +2,15 @@
 
 if [ ! -f /var/www/html/config/config.php ]
 then
-    NEXTCLOUD_ADMIN_PASSWORD=admin NEXTCLOUD_ADMIN_USER=admin SQLITE_DATABASE=True NEXTCLOUD_TRUSTED_DOMAINS="localhost passman.cc demo.passman.cc" NEXTCLOUD_UPDATE=1 /entrypoint.sh echo "Installation done"
+    # see https://github.com/nextcloud/docker/tree/master/26/apache/config/autoconfig.php
+    NEXTCLOUD_ADMIN_PASSWORD=admin NEXTCLOUD_ADMIN_USER=admin SQLITE_DATABASE=True NEXTCLOUD_TRUSTED_DOMAINS="localhost passman.cc demo.passman.cc" NEXTCLOUD_UPDATE=1 /entrypoint.sh echo "Installation complete"
 
     su -p www-data -s /bin/sh -c "cd /var/www/html/ php occ check &&
-        php occ status &&
-        php occ app:list &&
-        php occ app:install passman &&
         php occ app:disable firstrunwizard &&
-        php occ upgrade &&
-        php occ config:system:set defaultapp --value=passman"
+        php occ app:install passman &&
+        php occ config:system:set defaultapp --value=passman &&
+        php occ config:system:set appstoreenabled --type boolean --value=false"
+    echo "Preconfiguration complete"
 fi
 
 exec apache2-foreground
